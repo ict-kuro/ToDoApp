52bfb45f28db29c3aa7004319b75c5503324d2ff
ict-kuro <j847001y@mails.cc.ehime-u.ac.jp>
2022-09-08 17:40:57 +0900

    ログイン機能追加

--- master/TodoApp_Master/TodoApp_Master/Models/TodoAppContext.cs
+++ master/TodoApp_Master/TodoApp_Master/Models/TodoAppContext.cs
@@ -22,7 +22,7 @@ namespace TodoApp_Master.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("data source=192.168.28.62;initial catalog=TodoApp;User ID=sa;Password=IctxIct0;TrustServerCertificate=True;");
             }
         }
